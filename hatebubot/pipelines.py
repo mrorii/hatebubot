2c76ff3a13196e9301f5bccffdf00f016e50321b
Naoki Orii <mrorii@gmail.com>
2015-10-08 16:36:33 -0700

    Add initial draft

--- hatebubot/pipelines.py
+++ hatebubot/pipelines.py
@@ -5,7 +5,73 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
 
-class HatebubotPipeline(object):
+from scrapy import signals
+from scrapy.exporters import BaseItemExporter
+from scrapy.xlib.pydispatch import dispatcher
+
+
+class PrettyFloat(float):
+    '''
+    Used to format floats in json
+    http://stackoverflow.com/a/1733105
+    '''
+    def __repr__(self):
+        return '%.2g' % self
+
+
+def convert_to_utf8(json_obj):
+    '''
+    Converts simple json python representations to utf-8 recursively.
+    Refer to:
+    - http://stackoverflow.com/a/13105359
+    - http://stackoverflow.com/q/18337407
+    '''
+    if isinstance(json_obj, dict):
+        return dict((convert_to_utf8(key), convert_to_utf8(value))
+                    for key, value in json_obj.iteritems())
+    elif isinstance(json_obj, list):
+        return [convert_to_utf8(element) for element in json_obj]
+    elif isinstance(json_obj, unicode):
+        return json_obj.encode('utf-8')
+    elif isinstance(json_obj, float):
+        return PrettyFloat(json_obj)
+    else:
+        return json_obj
+
+
+class UnicodeJsonLinesItemExporter(BaseItemExporter):
+    '''
+    Prints out JSON in utf8 symbols, not their code points.
+    Refer to https://groups.google.com/forum/#!topic/scrapy-users/rJcfSFVZ3O4
+    '''
+    def __init__(self, file, **kwargs):
+        self._configure(kwargs)
+        self.file = file
+        self.encoder = json.JSONEncoder(ensure_ascii=False,
+                                        separators=(',', ':'),
+                                        **kwargs)
+
+    def export_item(self, item):
+        itemdict = dict(self._get_serialized_fields(item))
+        self.file.write(self.encoder.encode(convert_to_utf8(itemdict)) + '\n')
+
+
+class CustomJsonLinesItemPipeline(object):
+    def __init__(self):
+        dispatcher.connect(self.spider_opened, signal=signals.spider_opened)
+        dispatcher.connect(self.spider_closed, signal=signals.spider_closed)
+
+    def spider_opened(self, spider):
+        self.file = open('entries.json', 'w+b')
+        self.exporter = UnicodeJsonLinesItemExporter(self.file)
+        self.exporter.start_exporting()
+
+    def spider_closed(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
     def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
--- hatebubot/spiders/hatebu.py
+++ hatebubot/spiders/hatebu.py
@@ -1,13 +1,71 @@
 # -*- coding: utf-8 -*-
-import scrapy
+
+import urllib
+
+from scrapy.selector import Selector
+from scrapy.spiders import CrawlSpider, Rule
+
+from hatebubot.items import HatebuEntry
 
 
-class HatebuSpider(scrapy.Spider):
+def convert_to_int_if_int(s):
+    try:
+        return int(s)
+    except ValueError:
+        return s
+
+
+def generate_paginated_urls(params):
+    for of in xrange(0, params['num_hits'], params['window']):
+        yield 'http://b.hatena.ne.jp/search/tag?{0}'.format(urllib.urlencode({
+            'safe': params['safe'],
+            'q': params['tag'].encode('utf8'),
+            'users': params['users'],
+            'of': of,
+        }))
+
+
+class HatebuSpider(CrawlSpider):
     name = "hatebu"
     allowed_domains = ["b.hatena.ne.jp"]
-    start_urls = (
-        'http://www.b.hatena.ne.jp/',
-    )
+    download_delay = 1.0
+
+    params = {
+        'tag': u'ラーメン',
+        'users': 30,
+        'safe': 'off',
+        'num_hits': 5367,
+        'window': 40,
+    }
+
+    start_urls = generate_paginated_urls(params)
 
     def parse(self, response):
-        pass
+        selector = Selector(response)
+
+        entries = []
+
+        for result in selector.xpath("//li[@class='search-result']"):
+            entry = HatebuEntry()
+
+            is_ad = bool(result.xpath("p[@class='search-native-ad-text']"))
+            if is_ad:
+                continue
+
+            title = result.xpath("h3[1]/a/text()").extract()[0]
+            url = result.xpath("h3[1]/a/@href").extract()[0]
+            tags = result.xpath("div/div[@class='tags']/a/text()").extract()
+            category = result.xpath("div[@class='entryinfo']/a[1]/text()").extract()[0]
+            num_users = convert_to_int_if_int(result.xpath(".//span[@class='users']/strong/a/span/text()").extract()[0])
+            create_date = result.xpath(".//div[@class='entryinfo']/blockquote/span[@class='created']/text()").extract()[0]
+
+            entry['title'] = title
+            entry['url'] = url
+            entry['tags'] = tags
+            entry['category'] = category
+            entry['num_users'] = num_users
+            entry['create_date'] = create_date
+
+            entries.append(entry)
+
+        return entries
